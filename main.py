0b5dc4ff1ef4239d9470401e046f70c5c449f8a1
Ger0r0r <fedor.bombax@gmail.com>
2023-12-10 20:27:37 +0300

    update main, add result.csv

--- main.py
+++ main.py
@@ -16,26 +16,28 @@ def f1(x, a, b, c):
 
 ##################################################
 
+# Изменяемые параметры модели
 L=3
 K=3
 N=3
 
-# print("N = ", end="")
-# N = int(input())
-# print("K = ", end="")
-# K = int(input())
-# print("L = ", end="")
-# L = int(input())
+print("N = ", end="")
+N = int(input())
+print("K = ", end="")
+K = int(input())
+print("L = ", end="")
+L = int(input())
 
-debug = 0
-bad_s = 1000
-bad_i = 1000000
-border = 1000000
-max_steps = 10
+debug = 0 # flag for debug
+bad_s = 1000 # if during this count of iter e have no changes in A & B matrices, we enable debug flag automatically
+bad_i = 1000000 #for debug mode, if i > bad_i, we enable debug flag automitically
+border = 1000000 # if i - iter of sync lean inside of [1, border] we write syncronization succcess, else inc overborder var (below)
+
+CountOfExperiments = 10 # count of experiments (for one set of NKL)
 
 ##################################################
 
-# set random array
+# set random array from 0 to 1
 def gen_message ():
 	mes = np.zeros((K,N))
 	for i in range(N):
@@ -81,6 +83,7 @@ def calc_answer (m, s):
 # return number to range
 def return_in_border(T):
 	for i in range(N):
+		# some questions about method of bordering
 		for j in range(K):
 			if (T[j][i] > L):
 				T[j][i] = L
@@ -92,68 +95,11 @@ def return_in_border(T):
 
 # main function
 def change_coefficient (a, b, e, g, m):
-	# copies of A, B (will be changed)
-	new_a = a.copy()
-	new_b = b.copy()
-	new_e = e.copy()
-	new_g = g.copy()
-
-	# secret values of A, B
-	i_a = get_inter(m, a)
-	i_b = get_inter(m, b)
-	i_e = get_inter(m, e)
-	i_g = get_inter(m, g)
-
-	# results of A, B
-	r_a = np.prod(i_a)
-	r_b = np.prod(i_b)
-	r_e = np.prod(i_e)
-	r_g = np.prod(i_g)
-
-	if (debug):
-		print(str(a.tolist())+" "+str(i_a)+" "+str(r_a))
-		print(str(b.tolist())+" "+str(i_b)+" "+str(r_b))
-
-	for i in range(K):
-		# new_a[i,:] = a[i,:] + i_a[i]*m[i,:]*np.heaviside(r_a*i_a[i],0)*np.heaviside(r_a*r_b,0)
-		# new_b[i,:] = b[i,:] + i_b[i]*m[i,:]*np.heaviside(r_b*i_b[i],0)*np.heaviside(r_a*r_b,0)
-		new_a[i,:] = a[i,:] + i_a[i]*m[i,:]*(r_a==r_b and r_a==i_a[i])
-		new_b[i,:] = b[i,:] + i_b[i]*m[i,:]*(r_b==r_a and r_b==i_b[i])
-		if (debug):
-			print("a"+str(i)+" = "+str(a[i,:])+" + "+str(i_a[i])+" * "+str(m[i,:])+" * "+str(np.heaviside(r_a*i_a[i],0))+" * "+str(np.heaviside(r_a*r_b,0))+" = "+str(new_a[i,:]))
-			print("b"+str(i)+" = "+str(b[i,:])+" + "+str(i_b[i])+" * "+str(m[i,:])+" * "+str(np.heaviside(r_b*i_b[i],0))+" * "+str(np.heaviside(r_a*r_b,0))+" = "+str(new_b[i,:]))
-
-	for i in range(K):
-		new_e[i,:] = e[i,:] + i_e[i]*m[i,:]*(r_a==r_b and r_a==r_e and r_a==i_e[i])
-
-	if (r_g==r_a and r_g==r_b):
-		for i in range(K):
-			new_g[i,:] = g[i,:] + i_g[i]*m[i,:]*(r_g==i_g[i])
-	elif (r_g==r_a and r_g!=r_b):
-		index = np.argmin(np.abs(i_g))
-		new_g[index,:] = g[index,:] + i_g[index]*m[index,:]
-
-
-	new_a = return_in_border(new_a)
-	new_b = return_in_border(new_b)
-	new_e = return_in_border(new_e)
-	new_g = return_in_border(new_g)
-
-	if (debug):
-		print(str(new_a.tolist()))
-		print(str(new_b.tolist()))
-
-	return new_a, new_b, new_e, new_g, int(r_a == r_b)
+	
 
 #-------------------------------------------------
 
-
-def bin_array(num, m):
-	return np.array(list(np.binary_repr(num).zfill(m))).astype(np.int8)
-
-#-------------------------------------------------
-
-# debug function - print posible message that change secret
+# debug function - print possible message that change secret for case which we have no changes in A & B for long time(iterations)
 def find_changed_message(a, b):
 	t_a = a.copy()
 	t_b = b.copy()
@@ -175,6 +121,8 @@ def find_changed_message(a, b):
 	return 
 
 ##################################################
+## 						MAIN					##
+##################################################
 
 over_border = np.zeros(5)
 p_i = np.zeros((border, 5),dtype=int)
@@ -182,11 +130,12 @@ print(np.shape(p_i))
 p_l = np.zeros(2*L+1)
 p_r = np.zeros(3)
 
-steps = np.arange(0,max_steps)
-bar = IncrementalBar('Done', max = max_steps)
+steps = np.arange(0,CountOfExperiments)
+bar = IncrementalBar('Done', max = CountOfExperiments)
 
 bad_work = 0
 
+# step of syncronization between i and j 
 s_ab = 0
 s_ae = 0
 s_be = 0
@@ -280,6 +229,7 @@ for k in steps:
 
 		i = i + 1
 
+		# check of sync
 		if ((A == B).all() and s_ab == 0):
 			s_ab = i
 		if ((A == E).all() and s_ae == 0):
@@ -424,8 +374,10 @@ except FileNotFoundError:
 if (found == 0):
 	rows = len(data_R[0,:])
 	print("ROWS",str(rows))
+	# THINK ABOUT IT!
+	# недописана проверка на существование эксперимента с такими значениями NKL
 	if (rows == 1):
-		data_R[3] = data_R[3] + max_steps
+		data_R[3] = data_R[3] + CountOfExperiments
 		data_R[4] = data_R[4] + bad_work
 		data_R[5] = data_R[5] + over_border[0]
 		data_R[6] = data_R[6] + over_border[1]
@@ -436,7 +388,7 @@ if (found == 0):
 		for i in range(rows):
 			if ((data_R[i,0] == L) and (data_R[i,1] == K) and (data_R[i,2] == N)):
 				found = 1
-				data_R[i,3] = data_R[i,3] + max_steps
+				data_R[i,3] = data_R[i,3] + CountOfExperiments
 				data_R[i,4] = data_R[i,4] + bad_work
 				data_R[i,5] = data_R[i,5] + over_border[0]
 				data_R[i,6] = data_R[i,6] + over_border[1]
@@ -445,10 +397,10 @@ if (found == 0):
 				data_R[i,9] = data_R[i,9] + over_border[4]
 				break
 else:
-	data_R = np.array([L,K,N,max_steps,bad_work,over_border[0],over_border[1],over_border[2],over_border[3],over_border[4]])
+	data_R = np.array([L,K,N,CountOfExperiments,bad_work,over_border[0],over_border[1],over_border[2],over_border[3],over_border[4]])
 
 if (found == 0):
-	temp = np.array([L,K,N,max_steps,bad_work,over_border[0],over_border[1],over_border[2],over_border[3],over_border[4]])
+	temp = np.array([L,K,N,CountOfExperiments,bad_work,over_border[0],over_border[1],over_border[2],over_border[3],over_border[4]])
 	data_R = np.vstack((data_R,temp))
 	data_R = data_R[data_R[:, 2].argsort()]
 	data_R = data_R[data_R[:, 1].argsort()]
